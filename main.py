991fe0fd4e071eba9c5bd0c3e3e9abd18ef2619a
xianchen2 <31517904+xianchen2@users.noreply.github.com>
2020-03-18 23:39:55 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+
 import logging
 import os
 from sys import argv
@@ -16,21 +17,6 @@ def creat_and_update_index(index_name, doc_type):
         es.indices.creat(index=index_name)
     except Exception:
         pass    
-    es.indices.put_mapping(
-        index = index_name,
-        doc_type = doc_type,
-        body={
-            doc_type: { 
-            'properties':{
-                "Fine_amount": {"type": "float"},
-                "Penalty_amount": {"type": "float"},
-                "Interest_amount": {"type": "float"},
-                "Reduction_amount": {"type": "float"},
-                "Payment_amount": {"type": "float"},
-                "Amount_due": {"type": "float"}}
-            }
-        }
-    )
     return es
 
 def get_violate_data(filename):
@@ -40,20 +26,42 @@ def get_violate_data(filename):
     del d[-1]
     for i in range(len(d)):
         d[i] = ast.literal_eval(d[i])
-
     return d
 
 if __name__ == '__main__':
     #step 1: create an elastic search index 
-    es = creat_and_update_index('violations-index','parking')
+    es = creat_and_update_index('violation-index','parking')
     # step 2: get data
     docks = get_violate_data(argv[1])
     #push data into the elastic search
     for dock in docks:
         dock['issue_date'] = datetime.strptime(dock['issue_date'],'%m/%d/%Y') 
-        res = es.index(index='violations-index',doc_type='parking',body=dock)
+        try:
+            dock['fine_amount']=float(dock['fine_amount'])
+        except Exception:
+            pass    
+        
+        try:
+            dock['reduction_amount']=float(dock['reduction_amount'])
+        except Exception:
+            pass            
+
+        try:
+            dock['penalty_amount']=float(dock['penalty_amount'])
+        except Exception:
+            pass            
+        
+        try:
+            dock['interest_amount']=float(dock['interest_amount'])
+        except Exception:
+            pass            
+
+        try:
+            dock['amount_due']=float(dock['amount_due'])
+        except Exception:
+            pass               
+
+        res = es.index(index='violation-index',doc_type='parking',body=dock)
         print(res['result'])
-
-
 
 
